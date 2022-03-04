aebf962f9c53056a0257be6a3903bfcb52662abe
Nik Tomazic <nick.tomazic@gmail.com>
2022-03-04 19:18:49 +0100

    init_db now populates the database only if it is empty

--- init_db.py
+++ init_db.py
@@ -5,14 +5,15 @@ from app import Movie
 
 db.create_all()
 
-movies = [
-    Movie(title='Fight Club', release_date=date(1999, 9, 15)),
-    Movie(title='The Matrix', release_date=date(1999, 3, 31)),
-    Movie(title='Donnie Darko', release_date=date(2001, 1, 19)),
-    Movie(title='Inception', release_date=date(2010, 7, 16)),
-]
+if Movie.query.count() == 0:
+    movies = [
+        Movie(title='Fight Club', release_date=date(1999, 9, 15)),
+        Movie(title='The Matrix', release_date=date(1999, 3, 31)),
+        Movie(title='Donnie Darko', release_date=date(2001, 1, 19)),
+        Movie(title='Inception', release_date=date(2010, 7, 16)),
+    ]
 
-for movie in movies:
-    db.session.add(movie)
+    for movie in movies:
+        db.session.add(movie)
 
-db.session.commit()
+    db.session.commit()
