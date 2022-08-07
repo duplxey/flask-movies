4eccbdf61c116d2b94b33c90eb053695f0535271
hadiudoit11 <85430282+hadiudoit11@users.noreply.github.com>
2022-08-06 23:57:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5b3cd5b80eb8b217c20fb37074ff4a33'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///default.db'
-db = SQLAlchemy(app)
-
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), unique=True, nullable=False)
-    release_date = db.Column(db.Date(), nullable=False)
-
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-    def __repr__(self):
-        return '<Movie %r>' % self.title
+# app.config['SECRET_KEY'] = '5b3cd5b80eb8b217c20fb37074ff4a33'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///default.db'
+# db = SQLAlchemy(app)
+#
+#
+# class Movie(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(128), unique=True, nullable=False)
+#     release_date = db.Column(db.Date(), nullable=False)
+#
+#     def as_dict(self):
+#         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+#
+#     def __repr__(self):
+#         return '<Movie %r>' % self.title
 
 
 @app.route('/')
@@ -25,7 +25,7 @@ def index():
     return 'flask-movies'
 
 
-@app.route('/api/movies')
+@app.route('/api/moviesd')
 def api_movies():
     json = [movie.as_dict() for movie in Movie.query.all()]
     return jsonify(json)
